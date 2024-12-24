0a10b70ca2f4f494e95b012f0325033131feb967
chhotugoud7 <chhotu957588@gmail.com>
2024-12-24 16:46:21 +0530

    my-profile page is complete

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useState } from 'react';
-import Loginpage from '../components/Loginpage'
 
 const Login = () => {
   // const [isLogin, setIsLogin] = useState(true);
