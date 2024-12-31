717428b3fa6ba58998246a777b66a3c96b1a3603
chhotugoud7 <chhotu957588@gmail.com>
2024-12-31 23:37:59 +0530

    api endpoint is created for adminRouter to add pandit by admin

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
 
 
 
@@ -20,6 +21,9 @@ app.use(cors())
 
 
 //api endpoints
+app.use('/api/admin',adminRouter)
+// localhost: 4000/api/admin/add-pandit
+
 
 app.get('/', (req,res)=>{
     res.send('API Working great')
