402dc92c6425fffee96d71d4ec815fb1dca3b0bc
chhotugoud7 <chhotu957588@gmail.com>
2024-12-31 23:35:56 +0530

    here two routes are created one is adminRoute and second is panditRoute

---
+++ backend/routes/adminRoute.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { addPandit } from "../controllers/adminController.js";
+import upload from "../middlewares/multer.js";
+
+
+const adminRouter =express.Router()
+
+adminRouter.post('/addPandit',upload.single('image'),addPandit)
+
+export default adminRouter
