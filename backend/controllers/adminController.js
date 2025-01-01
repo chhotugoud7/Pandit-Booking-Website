69e544d0b3db3b7f5875d80f8c6620dc043d164d
chhotugoud7 <chhotu957588@gmail.com>
2025-01-01 22:36:16 +0530

    adding details to api for adding new pandit

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,4 +1,7 @@
-
+import validator from "validator"
+import bcrypt from 'bcrypt'
+import { v2 as cloudinary } from "cloudinary"
+import panditModel from "../models/panditModel.js"
 
 
 // API for adding pandit
@@ -15,10 +18,50 @@ const addPandit = async (req, res) => {
         }
 
         //validating email format
-        
+        if (!validator.isEmail(email)) {
+           return res.json({success:false,message:"Please enter a valid email"})                     
+
+        }
+
+        //validating strong password
+        if (password.length < 8){
+            return res.json({success:false,message:"Please enter a strong Password"})                     
+        }
+
+
+        //hashing pandit password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        //upload image to cloudinary
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type:"image"})
+        const imageUrl = imageUpload.secure_url
+
+
+        //pandit data
+        const panditData = {
+            name,
+            email,
+            image:imageUrl,
+            password:hashedPassword,
+            categories,
+            experience,
+            about,
+            fees,
+            address:JSON.parse(address),
+            date:Date.now()
+        }
+
+        const newPandit = new panditModel(panditData)
+        await newPandit.save()
+
+        res.json({success:true, message:"Pandit Added"})
+
 
     } catch(error) {
 
+        console.log(error)
+        res.json({success:false, message:error.message})
     }
 }
 
