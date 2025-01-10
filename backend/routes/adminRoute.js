7ca6f6580da82d6fdf61a91e0f8c96c0efaf855d
chhotugoud7 <chhotu957588@gmail.com>
2025-01-10 15:53:13 +0530

    /login is added for the admin login api in admin route

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { addPandit } from "../controllers/adminController.js";
+import { addPandit, loginAdmin } from "../controllers/adminController.js";
 import upload from "../middlewares/multer.js";
+import authAdmin from "../middlewares/authAdmin.js";
 
 
 const adminRouter = express.Router()
 
-adminRouter.post('/addPandit', upload.single('image'), addPandit)
+adminRouter.post('/add-pandit',authAdmin,upload.single('image'), addPandit)
+
+adminRouter.post('/login', loginAdmin)
+
 
 export default adminRouter
