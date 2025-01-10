38a3d4e409435218c153e1ef7def2d6e9d5f31e1
chhotugoud7 <chhotu957588@gmail.com>
2025-01-10 11:29:41 +0530

    mounting  context files to main.jsx

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,9 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import AdminContextProvider, { AdminContext } from './context/adminContext.jsx'
+import PanditContextProvider from './context/PanditContext.jsx'
+import AppContextProvider from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <AdminContextProvider>
+      <PanditContextProvider>
+        <AppContextProvider>
+
+          <App />
+          
+        </AppContextProvider>
+      </PanditContextProvider>
+    </AdminContextProvider>
   </BrowserRouter>,
 )
