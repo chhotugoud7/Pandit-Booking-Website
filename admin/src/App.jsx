b09411269052febf1294528dca0d18e2fe1ee74f
chhotugoud7 <chhotu957588@gmail.com>
2025-01-24 16:20:00 +0530

    update apps

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Login from './pages/Login.jsx'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/ReactToastify.css'
+import { AdminContext } from './context/adminContext.jsx';
+
 
 const App = () => {
-  return (
-    <div className='text-green-400'>App</div>
+
+  const {aToken} = useContext(AdminContext)
+
+  return aToken ? (
+    <div >
+
+      <ToastContainer/>
+
+    </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer/>
+    </>
   )
 }
 
